afd74b84a111dea841fb132c4ae49bab6d28894a
utek <lcpnine@gmail.com>
2021-01-08 23:38:33 +0900

    style: apply page style to Component in _app.tsx, flex(column direction)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app';
 import { FC } from 'react';
 import '../styles/global.scss';
+import '../styles/containers.scss';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return <Component className="page" {...pageProps} />;
 };
 
 export default MyApp;
