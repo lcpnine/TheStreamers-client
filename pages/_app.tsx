fc2cca73fa27c3e4b5d004cd40345775bb48ea52
utek <lcpnine@gmail.com>
2021-01-08 22:58:02 +0900

    feat: add style feat with scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { FC } from 'react';
+import '../styles/global.scss';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
