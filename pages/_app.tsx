0c9e304262ba4af3d280291ea3428732fe2c9295
utek <lcpnine@gmail.com>
2021-01-08 22:43:10 +0900

    feat: add custom _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from 'next/app';
+import { FC } from 'react';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
